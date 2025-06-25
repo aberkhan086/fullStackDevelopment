06d999c91d4ed9922d70e752cc70ac5b98525c1b
aberkhan086 <aberkhan086@gmail.com>
2025-06-25 16:42:56 +0530

    dombasics

---
+++ Dom.js
@@ -0,0 +1,8 @@
+document.title = "Dom in javascript";
+document.body.style.backgroundColor="lightblue";
+document.getElementById("heading1").innerHTML="DOM in MRCET";
+document.getElementById("para1").style.color="blue";
+document.getElementById("heading2").style.fontSize="20px";
+document.getElementsByClassName("list")[0].style.color="blue";
+document.getElementsByClassName("list")[1].style.color="darkgreen";
+document.getElementsByClassName("list")[2].style.color="white";
